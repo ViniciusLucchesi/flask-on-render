e96188b4f2a7c6e0fc2917977ed83c428a600ab8
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-11-19 16:38:48 -0300

    Add Flask routes for index and API

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api')
+def api():
+    return jsonify({'message': 'Hello, World!'})
